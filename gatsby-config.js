612300d5c0c8542bd27afef730545826cddb08ff
Dave Powers <djpowers89@gmail.com>
2020-05-23 16:54:00 -0400

    Block site from being indexed
    - add robots meta tags
    - https://support.google.com/webmasters/answer/93710?hl=en

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Movie Club Ratings`,
+    description: `Ratings for the films watched in the weekly movie club`,
     author: `@gatsbyjs`,
+    robots: `noindex,nofollow`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
