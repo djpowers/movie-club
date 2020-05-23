53319f364c8fb3c2bfbac4a6ec5cc79971a7a9c2
Dave Powers <djpowers89@gmail.com>
2020-05-23 15:16:35 -0400

    Implement basic bar chart with data
    - import JSON data (https://www.gatsbyjs.org/docs/sourcing-content-from-json-or-yaml/#directly-import-data-with-json)
    - use nivo to generate bar chart (https://nivo.rocks/bar/)
    - set height on parent container to ensure chart is rendered

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import React from "react"
+import JSONData from "../../content/movie-ratings.json"
 import { Link } from "gatsby"
+import { ResponsiveBar } from "@nivo/bar"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
@@ -8,13 +10,40 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <ResponsiveBar
+      data={JSONData.content}
+      indexBy="title"
+      keys={["DP", "JG", "ML", "JS"]}
+      groupMode="grouped"
+      minValue="0"
+      maxValue="100"
+      margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
+      colors={{ scheme: "nivo" }}
+      legends={[
+        {
+          dataFrom: "keys",
+          anchor: "bottom-right",
+          direction: "column",
+          justify: false,
+          translateX: 120,
+          translateY: 0,
+          itemsSpacing: 2,
+          itemWidth: 100,
+          itemHeight: 20,
+          itemDirection: "left-to-right",
+          itemOpacity: 0.85,
+          symbolSize: 20,
+          effects: [
+            {
+              on: "hover",
+              style: {
+                itemOpacity: 1,
+              },
+            },
+          ],
+        },
+      ]}
+    />
   </Layout>
 )
 
